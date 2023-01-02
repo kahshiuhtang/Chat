11813e4e794a5414c46941d7695316bc4f549b4b
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-01-01 21:31:04 -0500

    Worked on the message display of individual chat

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="message-info">
+        <img src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"></img>
+        <span>Just Now</span>
+      </div>
+      <div className="message-content">
+        <p>Hello World</p>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
