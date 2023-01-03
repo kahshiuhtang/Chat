d12ad65cd8cbba406e611f9d9f90f215c55eae7b
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2023-01-03 17:31:08 -0500

    Worked on sending the actual messages from database

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,10 +2,12 @@ import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
   useEffect(() => {
     const getChats = () => {
       const unsub = onSnapshot(
@@ -20,10 +22,17 @@ const Chats = () => {
     };
     currentUser.uid && getChats();
   }, [currentUser.uid]);
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
   return (
     <div className="chats">
       {Object.entries(chats)?.map((chat) => (
-        <div className="user-chat" key={chat[0]}>
+        <div
+          className="user-chat"
+          key={chat[0]}
+          onClick={handleSelect(chat[1].userInfo)}
+        >
           <img src="https://t3.ftcdn.net/jpg/02/99/04/20/360_F_299042079_vGBD7wIlSeNl7vOevWHiL93G4koMM967.jpg"></img>
           <div className="user-chat-info">
             <span>{chat[1].userInfo.username}</span>
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
 import Message from "./Message";
 
 const Messages = () => {
+  const { data } = useContext(ChatContext);
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatID), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+    return () => {
+      unSub();
+    };
+  }, [data.chatID]);
   return (
     <div className="messages">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
